8eeb37c9ddd5653544a02e4010d7a85f326835f0
Spandana2703 <92169664+Spandana2703@users.noreply.github.com>
2025-02-12 14:03:09 +0530

    Update Fixed.cpp

--- Day1/IntegerOverflow/Fixed.cpp
+++ Day1/IntegerOverflow/Fixed.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <limits>
+
+bool safe_add(int a, int b, int &result) {
+    if((b>0 && a> std::numeric_limits<int>::max()-b) || (b<0 && a< std::numeric_limits<int>::min()-b)) 
+    {
+        return false;
+    }
+    result =a+b;
+    return true;
+}
+
+int main() {
+    
+    //int x = 2000000000000;
+    //int y = 1500000000000;
+    //int result;
+    int x,y,result;
+    std::cout << "Enter the first integer (x): ";
+    std::cin >> x;
+    std::cout << "Enter the second integer (y): ";
+    std::cin >> y;
+    
+    if(safe_add(x,y,result))
+    {
+        std::cout << "Result: "<< result << std::endl;
+    }
+    else
+    {
+        std::cout<<"Integer Overflow";
+    }
+    return 0;
+    
+ 
+}
 
