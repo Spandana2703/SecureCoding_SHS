1b3bd67e27dfe3d95291af9de6b600eced6f5c29
Spandana2703 <92169664+Spandana2703@users.noreply.github.com>
2025-02-12 16:31:29 +0530

    Create Fixed- vulnerable-app.js

---
+++ Day1/CommandInjection/Fixed- vulnerable-app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { exec, execFile } = require('child_process');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Ping a Website</h1>
+        <form method="POST" action="/ping">
+            <label for="host">Enter a hostname or IP address:</label>
+            <input type="text" id="host" name="host" placeholder="e.g., google.com">
+            <button type="submit">Ping</button>
+        </form>
+    `);
+});
+
+app.post('/ping', (req, res) => {
+    const host = req.body.host;
+
+   
+    if (!/^[a-zA-Z0-9.-]+$/.test(host)) {
+        return res.send('Invalid hostname or IP address');
+    } 
+
+    execFile('ping', ['-c', '4', host], (error, stdout, stderr) => {
+        if (error) {
+            return res.send(`Error: ${stderr}`);
+        }
+        res.send(`<pre>${stdout}</pre>`);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('ping app running on http://localhost:3000');
+});
